9f2def23207ae6c9488a1431a93a8982085effa9
Nikola Đuza <nikolaseap@gmail.com>
2021-04-18 13:15:40 +0200

    Use :sidekiq adapter for ActiveJob

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module DuplicateSidekiqJobs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.active_job.queue_adapter = :sidekiq
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
